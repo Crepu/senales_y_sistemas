9c3035c4cc9ce2848cf04e45261498f495b0751b
Crepu <danielmendez310@gmail.com>
2018-09-07 13:23:40 -0300

    subplot example

--- ondas.py
+++ ondas.py
@@ -5,10 +5,10 @@ import math
 vmax = 20
 samp_rate = 1000
 altura = 1
-length = 1
+length = 2
 heigth = 1
 xi = 0
-xf = 10
+xf = 2
 delta = 1
 value = 1
 
@@ -34,13 +34,26 @@ sinx = lambda: [math.sin(x)/x for x in source(vmax,samp_rate)]
 )
 dec_exp = lambda xi,xf: [x for x in reversed(grow_exp(xi,xf))]
 """
+def grow_exp(xi, xf):
+    arr_1 = [0 for x in np.linspace(-vmax, xi, math.floor(samp_rate / 3))]
+    arr_2 = [math.exp(x) for x in np.linspace(
+                        xi, xf, math.ceil(samp_rate / 3))]
+    arr_3 = [0 for x in np.linspace(xf, vmax, math.floor(samp_rate / 3))]
+    arr_r = np.append(arr_1, arr_2)
+    return np.append(arr_r,arr_3)
+g_exp = grow_exp(xi,xf)
 dirac = lambda delta: [0 if x != samp_rate/2 else delta for x in range(samp_rate)]  
 step = lambda value: [0 if x < 0 else value for x in source(vmax,samp_rate)]
 
-#f_entrada = [seno, square(length,heigth), triang(altura)]
+f_entrada = [seno, square(length,heigth), triang(altura)]
 #f_transf = [dec_exp(xi,xf),grow_exp(xi,xf),dirac(delta),step(value),sinx]
-
-plt.plot(source(vmax,samp_rate),step(value))
+conv = np.convolve(square(length,heigth),g_exp,'same')
+plt.subplot(311)
+plt.plot(source(vmax,samp_rate),square(length,heigth))
+plt.subplot(312)
+plt.plot(source(vmax,samp_rate),g_exp)
+plt.subplot(313)
+plt.plot(source(vmax,samp_rate),conv)
 plt.xlabel('sample index')
 plt.ylabel('amplitude')
 plt.show();
